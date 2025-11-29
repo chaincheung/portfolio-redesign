f940d42605175554a0137cd10c555ea7e9ceeee0
John Cheung <johncheung@Johns-MacBook-Pro.local>
2025-11-29 05:22:20 -0500

    Remove scroll position retention on page navigation - scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PortfolioPage } from "./portfolio/PortfolioPage";
 import { ProjectDetail } from "./portfolio/ProjectDetail";
+import { ScrollToTop } from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<PortfolioPage />} />
         <Route path="/project/:projectSlug" element={<ProjectDetail />} />
